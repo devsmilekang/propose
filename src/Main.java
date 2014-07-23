b22e08c6bb516cbb002afa0a61e94965a7faca9e
kangminser <kangminser88@gmail.com>
2014-07-23 16:21:33 +0900

    메세지 입력 및 Main화면 메세지 가져오는 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	static public void main(String args[]){
 		GetData a = new GetData();
 		//http://121.254.179.10:8888/propose/getData.jsp?select=heartCount&myPhoneNumber=01025003712
-		a.setSelect("heartCount");
+		a.setSelect("main");
 		a.setMyPhoneNumber("01025003712");
 		a.dataSelect();
 	}
--- src/com/kms/propose/data/GetData.java
+++ src/com/kms/propose/data/GetData.java
@@ -20,8 +20,6 @@ public class GetData extends KmsDbApi{
 	private String select;
 	private String loveMsg;
 	
-
-
 	public GetData(){
 		if(!super.MysqlConn("121.254.179.10", "3306", "propose", "kms", "kms1234")){
 			this.obj.put("eroor", "1");
@@ -41,7 +39,7 @@ public class GetData extends KmsDbApi{
 					getLoveCount();		//내가 여지껏 받은 하트 개순
 					getMyPoint();		//내 포인트 가져오기
 					getHeartList();		//하트 리스트 가져오기		
-					getHeartCount();	//내하트개수 가져오기
+					getMainHeartCount();
 				}
 			}
 			else if("sendHeart".equals(select)){
@@ -113,11 +111,11 @@ public class GetData extends KmsDbApi{
 							insertLoevMember();
 						}
 						else{
-							if(checkItem("2") > 0){
+							if(checkItemCnt("2") > 0){
 								insertLoevMember();
 							}
 							else{
-								errorMsg("아이템이 부족합니다.");
+								errorMsg("아이템 부족합니다.");
 							}
 						}
 						
@@ -162,6 +160,7 @@ public class GetData extends KmsDbApi{
 		catch(Exception e){
 			e.printStackTrace();
 			obj.put("error", "1");
+			obj.put("msg", e.toString());
 		}
 		return obj.toString();
 	}
@@ -247,6 +246,26 @@ public class GetData extends KmsDbApi{
 		return cnt;
 	}
 	
+	public int getMainHeartCount(){
+		StringBuffer query = new StringBuffer();
+		List list = new ArrayList();
+		query.append(" select ifnull(k.heartCount,0)+ifnull(m.itemHeartCount,0) as heartCount from propose.member k  ");
+		query.append(" left outer join ");
+		query.append(" (select member_phone, COUNT(*) as itemHeartCount from propose.memberItem m where use_flag='0' and item_id in('0','1') ");
+		query.append(" and member_phone='").append(myPhoneNumber).append("') m ");
+		query.append(" on k.member_phone = m.member_phone ");
+		query.append(" where k.member_phone='").append(myPhoneNumber).append("'");
+		
+		list = super.executeQuery(query.toString());
+		int cnt =0;
+		for(int k=0; k<list.size(); k++){
+			Map map = (Map)list.get(k);
+			obj.put("heartCount", map.get("HEARTCOUNT"));
+			cnt = Integer.parseInt(String.valueOf(map.get("HEARTCOUNT")));
+		}
+		return cnt;
+	}
+	
 	public int getCheckMemberId(){
 		List list = new ArrayList();
 		StringBuffer query = new StringBuffer();
@@ -292,10 +311,11 @@ public class GetData extends KmsDbApi{
 		StringBuffer query = new StringBuffer();
 		query = new StringBuffer("");			//하트 정보 가져오기 sendflag -> 0 : 내가 보낸거, 1 :내가 받은거
 		query.append(" select case when s.heart_flag='2' then case when s.sendflag='0' then '3' else '4' end ");
-		query.append(" else s.sendflag end as sendflag, s.member_love_phone,s.member_phone,s.heart_send_time, s.heart_flag from ");
-		query.append(" ( select case when m.member_phone is not null then 2 ");
+		query.append(" else s.sendflag end as sendflag, s.member_love_phone,s.member_phone,s.heart_send_time, s.heart_flag ");
+		query.append(" , s.loveMsg, case when s.loveMsg is null then 0 else 1 end as msgFlag ");		
+		query.append(" from ( select case when m.member_phone is not null then 2 ");
 		query.append(" when k.member_phone = '").append(myPhoneNumber).append("' then 0 else 1 end as sendflag, ");
-		query.append(" k.member_love_phone, k.member_phone, DATE_FORMAT(k.heart_send_time,'%Y-%m-%d %H:%i:%S') as heart_send_time , k.heart_flag ");
+		query.append(" k.member_love_phone, k.member_phone, DATE_FORMAT(k.heart_send_time,'%Y-%m-%d %H:%i:%S') as heart_send_time , k.heart_flag,k.loveMsg ");
 		query.append(" from loveMember k left outer join loveMember m ");
 		query.append(" on k.member_phone = m.member_love_phone and k.member_love_phone = m.member_phone ");
 		query.append(" and k.heart_flag not in('2','3') and m.heart_flag not in('2','3') ");
@@ -321,6 +341,8 @@ public class GetData extends KmsDbApi{
 			data.put("member_love_phone", map.get("MEMBER_LOVE_PHONE"));
 			data.put("member_phone", map.get("MEMBER_PHONE"));
 			data.put("heart_send_time", map.get("HEART_SEND_TIME"));
+			data.put("msgFlag", map.get("MSGFLAG"));
+			data.put("loveMsg", map.get("LOVEMSG"));
 			jsonArr.add(data);
 		}
 		obj.put("heartList", jsonArr);
@@ -376,18 +398,25 @@ public class GetData extends KmsDbApi{
 		int cnt = super.executeUpdate(query.toString());
 		obj.put("insertFlag", cnt);
 		query = new StringBuffer("");
-		query.append(" update member set heartCount=heartCount-1 ");
+		query.append(" update member set heartCount=heartCount-1 ");		//하트감소
 		query.append(" where member_phone='").append(myPhoneNumber).append("'");
 		cnt = super.executeUpdate(query.toString());
 		obj.put("updateFlag", cnt);
-		if(!"".equals(loveMsg)){
+		if(!"".equals(loveMsg)){		//Msg 있으면 아이템 사용
 			query = new StringBuffer("");
-			//qeury.append("update ")
-			//query.append("update )
+			query.append(" update propose.memberItem a, ");
+			query.append(" (select * from propose.memberItem k where k.memberItemId = ");
+			query.append(" (select memberItemId from propose.memberItem where use_flag='0' ");
+			query.append(" and item_id='2' ");
+			query.append(" and member_phone='").append(myPhoneNumber).append("' ");
+			query.append(" limit 0,1)) b ");
+			query.append(" set a.use_flag='1', a.end_date=now() ");
+			query.append(" where a.memberItemId = b.memberItemId ");
+			super.executeUpdate(query.toString());
 		}
 	}
 	
-	public int checkItem(String itemId){
+	public int checkItemCnt(String itemId){
 		List list = new ArrayList();
 		StringBuffer query = new StringBuffer();
 		query.append(" SELECT item_id FROM propose.memberItem ");
